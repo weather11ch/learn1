213293d79aedaf3c7013b371c7836ba6c2460bac
rchudaeva <weather11ch@gmail.com>
2024-03-19 19:29:22 +0300

    Добавьте файлы проекта.

---
+++ learn1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace learn1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число a");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите число b");
+            int b = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("a + b = " + (a+b));
+            Console.ReadLine();
+        }
+    }
+}
