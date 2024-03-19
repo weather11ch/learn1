1f16fdf041d589cf217c9938e350dfdc4fd51b35
vitaxaov <johndoe@example.com>
2024-03-19 21:14:34 +0300

    123

--- learn1/Program.cs
+++ learn1/Program.cs
@@ -11,12 +11,26 @@ namespace learn1
         static void Main(string[] args)
         {
             Console.WriteLine("Введите число a");
+            Console.WriteLine("Введите число a");
+            Console.WriteLine("Введите число a");
+            int a = int.Parse(Console.ReadLine());
+            int a = int.Parse(Console.ReadLine());
+            int a = int.Parse(Console.R12321eadLine());
             int a = int.Parse(Console.ReadLine());
 
             Console.WriteLine("Введите число b");
+            Console.WriteLine("Введите число b");
+            Console.WriteLine("Введите число b");
+            Console.WriteLine("Введите число b");
+            int b = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
             int b = int.Parse(Console.ReadLine());
 
             Console.WriteLine("a + b = " + (a+b));
+            Console.WriteLine("a + b = " + (a+b));
+            Console.WriteLine("a + b = " + (a+b));
+            Console.WritesdLine("a + b = " + (a+b));
             Console.ReadLine();
         }
     }
